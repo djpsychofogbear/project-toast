90c91c3a599ded8a19dfe1feabda6d867784f70b
Theo Dixon <121321582+djpsychofogbear@users.noreply.github.com>
2025-10-01 00:07:21 -0400

    exercise(5.2): Add sr a11y diff to ToastShel and Toast

--- src/components/Toast/Toast.tsx
+++ src/components/Toast/Toast.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import {
-  AlertOctagon,
-  AlertTriangle,
-  CheckCircle,
-  Info,
-  X,
+    AlertOctagon,
+    AlertTriangle,
+    CheckCircle,
+    Info,
+    X,
 } from 'react-feather';
 
 import VisuallyHidden from '../VisuallyHidden';
@@ -14,36 +14,45 @@ import styles from './Toast.module.css';
 type ToastVariant = 'notice' | 'warning' | 'success' | 'error';
 
 export type ToastProps = {
-  id: string;
-  variant?: ToastVariant;
-  children?: React.ReactNode;
-  onDismiss?: () => void;
+    id: string;
+    variant?: ToastVariant;
+    children?: React.ReactNode;
+    onDismiss?: () => void;
 };
 
 const ICONS_BY_VARIANT = {
-  notice: Info,
-  warning: AlertTriangle,
-  success: CheckCircle,
-  error: AlertOctagon,
+    notice: Info,
+    warning: AlertTriangle,
+    success: CheckCircle,
+    error: AlertOctagon,
 } as const;
 
-function Toast({ variant = 'notice', children, onDismiss }: ToastProps): React.ReactElement {
-  const IconComponent = ICONS_BY_VARIANT[variant];
-  
-  return (
-    <div className={`${styles.toast} ${styles[variant]}`}>
-      <div className={styles.iconContainer}>
-        <IconComponent size={24} />
-      </div>
-      <p className={styles.content}>
-        {children}
-      </p>
-      <button className={styles.closeButton} onClick={onDismiss}>
-        <X size={24} />
-        <VisuallyHidden>Dismiss message</VisuallyHidden>
-      </button>
-    </div>
-  );
+function Toast({
+    variant = 'notice',
+    children,
+    onDismiss,
+}: ToastProps): React.ReactElement {
+    const IconComponent = ICONS_BY_VARIANT[variant];
+
+    return (
+        <div className={`${styles.toast} ${styles[variant]}`}>
+            <div className={styles.iconContainer}>
+                <IconComponent size={24} />
+            </div>
+            <p className={styles.content}>
+                <VisuallyHidden>{`${variant} -`}</VisuallyHidden>
+                {children}
+            </p>
+            <button
+                className={styles.closeButton}
+                onClick={onDismiss}
+                aria-label="Dismiss message"
+                aria-live="off">
+                <VisuallyHidden>Dismiss message</VisuallyHidden>
+                <X size={24} />
+            </button>
+        </div>
+    );
 }
 
-export default Toast; 
+export default Toast;
